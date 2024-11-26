f0d22b7afa74a6616a07fc769fb69a3c3ca8fec1
AnthonyMAURICE <116059905+AnthonyMAURICE@users.noreply.github.com>
2024-11-26 16:30:21 +0100

    Update FormObserver.cs

--- ConsoleObserver/WinFormsObserver/FormObserver.cs
+++ ConsoleObserver/WinFormsObserver/FormObserver.cs
@@ -24,6 +24,7 @@ namespace WinFormsObserver
             {
                 Test((DoubleAgent)person);
             }
+            
         }
 
         private void button2_Click(object sender, EventArgs e)
