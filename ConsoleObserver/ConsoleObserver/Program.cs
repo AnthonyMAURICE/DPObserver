cc74c01a7de44cd2408c4bbc236bc69a4cbf35fb
AnthonyMAURICE <116059905+AnthonyMAURICE@users.noreply.github.com>
2024-12-09 11:09:28 +0100

    Update Program.cs

--- ConsoleObserver/ConsoleObserver/Program.cs
+++ ConsoleObserver/ConsoleObserver/Program.cs
@@ -9,34 +9,22 @@ namespace ConsoleObserver
         {
             DoubleAgent spyOne = new("Bob");
             RegularCitizen regularOne = new("Bill");
+
+
             RegularCitizen regularTwo = new("Billy");
             Tourist touristOne = new("Billy-Bob");
             Tourist touristTwo = new("Bobby-Bill");
             Counterintelligence counterintelligence = new();
             SecretPolice secretPolice = new();
 
+            
             counterintelligence.ConnectTo(spyOne);
             counterintelligence.Disconnect();
-            regularOne.Subscribe(secretPolice);
-            //Other Subscriptions region
-            #region
             spyOne.Subscribe(secretPolice);
-            regularTwo.Subscribe(secretPolice);
-            regularOne.Subscribe(counterintelligence);
-            regularTwo.Subscribe(counterintelligence);
-            touristOne.Subscribe(counterintelligence);
-            touristTwo.Subscribe(counterintelligence);
-            #endregion
+            regularOne.Subscribe(secretPolice);
+
             spyOne.Encrypt();
             regularOne.Vote();
-            //Other actions region
-            #region
-            regularOne.Encrypt();
-            regularTwo.Encrypt();
-            regularTwo.Vote();
-            touristOne.Encrypt();
-            touristTwo.Encrypt();
-            #endregion
         }
     }
 }
